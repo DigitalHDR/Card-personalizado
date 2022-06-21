657b4d56c0d553cf22d15c6f608cdab09b13bbac
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-06-21 17:41:03 -0300

    pequena alteração

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const body = document.querySelector('body')
 body.addEventListener('click', () => {
-    const audio =  document.querySelector('audio')
-    return audio.play()
+  const audio = document.querySelector('audio')
+  return audio.play()
 })
